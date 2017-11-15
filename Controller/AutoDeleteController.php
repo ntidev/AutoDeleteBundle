87337a5b980734c8d7bf5c392397359ac584377b
Benjamin Vison <benjamin@syneteksolutions.com>
2017-11-15 04:48:44 -0500

    Added the empty controller

---
+++ Controller/AutoDeleteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NTI\AutoDeleteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class AutoDeleteController
+ */
+class AutoDeleteController extends Controller {
+
+}
